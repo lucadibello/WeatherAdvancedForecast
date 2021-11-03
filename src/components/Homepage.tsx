78c94d49673c771f36dbafb3b3b58ee967a53cc2
luca.dibello@student.supsi.ch <info@lucadibello.ch>
2021-11-03 14:13:31 +0100

    Updated homepage

--- src/components/Homepage.tsx
+++ src/components/Homepage.tsx
@@ -10,23 +10,44 @@ import {
 import UserLocation from '../models/UserLocation';
 import * as LocationService from '../services/LocationService';
 import * as WeatherService from '../services/WeatherService';
-import { LOCATION_CACHE_KEY } from '../constants/Cache';
+import { LOCATION_CACHE_KEY, CITY_CACHE_KEY } from '../constants/Cache';
 import * as CacheService from '../services/CacheService';
-
-
-
+import NearbyCity from '../models/NearbyCity';
 
 export default function Homepage () {
   const [location, setLocation] = React.useState<UserLocation | null>(null);
+  const [city, setCity] = React.useState<NearbyCity | null>(null);
 
-  const nearbyCities = () => {
-    if (location != null) {
-      WeatherService.getNearbyCities(
-        location.latitude,
-        location.longitude,
-        10
-      );
-    }
+  const updateExactLocation = () => {
+    LocationService.getUserLocation(async (position: GeolocationPosition) => {
+      // parse data
+      let locationData = {
+        latitude: position.coords.latitude,
+        longitude: position.coords.longitude,
+        altitude: position.coords.altitude,
+        _accuracy: position.coords.accuracy
+      }
+
+      // Update user location
+      setLocation(locationData)
+
+      // Update cache location
+      CacheService.cacheData(LOCATION_CACHE_KEY, JSON.stringify(locationData), LocationService.getUserLocation)
+      
+      // get nearby cities
+      let data = await WeatherService.getNearbyCities(position.coords.latitude, position.coords.longitude, 1);
+
+      // check if data has been found
+      if (data.length > 0) {
+        // Set city in react state
+        setCity(data[0]);
+        // Set city in cache
+        CacheService.cacheData(CITY_CACHE_KEY, JSON.stringify(data[0]), WeatherService.getNearbyCities)
+      } else {
+        // FIXME: add popup message / toast to notiy the user
+        console.log("ERROR: NO NEARBY CITY HAS BEEN FOUND")
+      }
+    })
   }
 
   // Try to update user location
@@ -34,49 +55,30 @@ export default function Homepage () {
   useEffect(() => {
     // Check for cached data
     const cachedLocationData = CacheService.getCachedData(LOCATION_CACHE_KEY);
+    const cachedCityData = CacheService.getCachedData(CITY_CACHE_KEY);
 
     // Check if data has been found
-    if (cachedLocationData != null) {
+    if (cachedLocationData != null && cachedCityData != null) {
       // Found cached data, no additional actions required
       setLocation(cachedLocationData.data);
+      setCity(cachedCityData.data);
     } else {
-      // Fetch user position
-      LocationService.getUserLocation((position: GeolocationPosition) => {
-        // Parse data into correct form
-        let locationData = {
-          latitude: position.coords.latitude,
-          longitude: position.coords.longitude,
-          altitude: position.coords.altitude,
-          _accuracy: position.coords.accuracy
-        }
-        
-        // save data into location state
-        setLocation(locationData);
-
-        // save data into user cache
-        CacheService.cacheData(LOCATION_CACHE_KEY, JSON.stringify(locationData), LocationService.getUserLocation)
-      })
+      // Fetch user position and city
+      updateExactLocation();
     }
   },[])
 
   return (
     <Box>
       {/* Ask user to enable location */}
-      {location === null && 
-        <Alert severity="error">
-          Enable your location to get the information you need in less time :)
-          <Button variant="outlined" color="error"  onClick={() => {
-            LocationService.getUserLocation((position: GeolocationPosition) => {
-              setLocation({
-                latitude: position.coords.latitude,
-                longitude: position.coords.longitude,
-                altitude: position.coords.altitude,
-                _accuracy: position.coords.accuracy
-              })
-            })
+      {LocationService.isLocationAvailable() && 
+        <Alert color={location==null ? "error" : "warning"}>
+          Update your location to get the information you need in less time :)
+          <Button variant="outlined" color={location==null ? "error" : "warning"}  onClick={() => {
+            updateExactLocation();
           }} sx={{
             marginLeft: "10px"
-          }}>Enable location</Button>
+          }}>{location === null ? "Set your location" : "Update your location"}</Button>
         </Alert>
       }
       
@@ -89,6 +91,10 @@ export default function Homepage () {
 
       <Typography variant='h6' component='h1'>
         Current position: {JSON.stringify(location)}
+      </Typography>
+      
+      <Typography variant='h6' component='h1'>
+        Detected location: {JSON.stringify(city)}
       </Typography>
     </Box>
   );
