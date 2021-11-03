53055210f6ee721794a2f6ef95535e13986d228d
luca.dibello@student.supsi.ch <info@lucadibello.ch>
2021-11-03 13:22:56 +0100

    Changed export (more compact)

--- src/models/UserLocation.ts
+++ src/models/UserLocation.ts
@@ -1,10 +1,6 @@
-interface UserLocation {
+export default interface UserLocation {
   latitude: number,
   longitude: number,
   altitude: number | null,
   _accuracy: number
-}
-
-export type {
-  UserLocation
 }
