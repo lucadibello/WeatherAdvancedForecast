5d58462c3659d0c540dc7967c7932ffe05d246cd
Armando <taglialatela.arma@gmail.com>
2021-11-03 19:56:49 +0100

    Add drawer menu

--- src/components/Draweropen.tsx
+++ src/components/Draweropen.tsx
@@ -54,16 +54,7 @@ export default function TemporaryDrawer() {
         ))}
       </List>
       <Divider />
-      <List>
-        {['All mail', 'Trash', 'Spam'].map((text, index) => (
-          <ListItem button key={text}>
-            <ListItemIcon>
-              {index % 2 === 0 ? <InboxIcon /> : <MailIcon />}
-            </ListItemIcon>
-            <ListItemText primary={text} />
-          </ListItem>
-        ))}
-      </List>
+      
     </Box>
   );
 
