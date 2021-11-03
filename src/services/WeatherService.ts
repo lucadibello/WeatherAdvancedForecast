b9cb47977fba75794d443adb4740c9c184a25d2e
luca.dibello@student.supsi.ch <info@lucadibello.ch>
2021-11-03 14:13:13 +0100

    Removed additional operations in WeatherService

--- src/services/WeatherService.ts
+++ src/services/WeatherService.ts
@@ -1,16 +1,9 @@
 import { API_GEOCODE_REVERSE_URL } from "../constants/OpenWeather"
+import NearbyCity from "../models/NearbyCity";
 
-async function getNearbyCities(lat:number, lon: number, nCities: number) {
-  await fetch(API_GEOCODE_REVERSE_URL+`?lat=${lat}&lon=${lon}&limit=${nCities}&appid=${process.env.REACT_APP_OPENWEATHER_TOKEN}`)
-    .then(response => response.json())
-    .then((response) => {
-      console.log("Response data: ", response);
-      if (response.length > 0) {
-        return response[0];
-      } else {
-        return null;
-      }
-    })
+async function getNearbyCities(lat:number, lon: number, nCities: number): Promise<NearbyCity[]> {
+  return await fetch(API_GEOCODE_REVERSE_URL+`?lat=${lat}&lon=${lon}&limit=${nCities}&appid=${process.env.REACT_APP_OPENWEATHER_TOKEN}`)
+    .then(response => response.json());
 }
 
 export {
