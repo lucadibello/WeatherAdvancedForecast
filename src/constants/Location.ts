b0c2b1056b869ae03642e433e951c0b2e9510cf7
luca.dibello@student.supsi.ch <info@lucadibello.ch>
2021-11-03 11:12:19 +0100

    Location related constants

---
+++ src/constants/Location.ts
@@ -0,0 +1,9 @@
+const IS_HIGH_ACCURACY_ENABLED = true;
+const TIMEOUT = 5000;
+const MAXIMUM_AGE = 0;
+
+export {
+  IS_HIGH_ACCURACY_ENABLED,
+  TIMEOUT,
+  MAXIMUM_AGE
+}
---
+++ src/models/LocationPermission.ts
@@ -0,0 +1,9 @@
+enum LocationPermission  {
+  GRANTED,
+  PROMPT,
+  DENIED,
+  NOT_AVAILABLE,
+  UNKNOWN_RESPONSE
+}
+
+export default LocationPermission;
