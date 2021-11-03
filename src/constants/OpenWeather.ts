50802417775001c9471ac7e3aef5b640f9218726
luca.dibello@student.supsi.ch <info@lucadibello.ch>
2021-11-03 13:08:06 +0100

    Added new constants

--- src/constants/OpenWeather.ts
+++ src/constants/OpenWeather.ts
@@ -1,5 +1,7 @@
-const API_URL = "pro.openweathermap.org/data/2.5/forecast";
+const API_FORECAST_URL = "http://pro.openweathermap.org/data/2.5/forecast";
+const API_GEOCODE_REVERSE_URL = "http://api.openweathermap.org/geo/1.0/reverse";
 
 export {
-  API_URL
+  API_FORECAST_URL,
+  API_GEOCODE_REVERSE_URL
 }
