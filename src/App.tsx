79800df387cf77448f5eb39c7a82eefef3ed0ef2
luca.dibello@student.supsi.ch <info@lucadibello.ch>
2021-11-03 15:21:44 +0100

    Added API key check in entrypoint

--- src/App.tsx
+++ src/App.tsx
@@ -4,18 +4,52 @@ import Homepage from './components/Homepage';
 import Navbar from './components/Navbar';
 
 import {
+  Alert,
+  AlertTitle,
   Container
 } from '@mui/material'
 
 function App() {
-  return (
-    <div className="App">
-      <Navbar />
-      <Container sx={{textAlign: 'left', paddingTop: '10px'}}>
-        <Homepage />
-      </Container>
-    </div>
-  );
+  // check if API token is present in environment variables
+  let tokenExists = Boolean(process.env.REACT_APP_OPENWEATHER_TOKEN)
+
+  if (tokenExists) {
+    return (
+      <div className="App">
+        <Navbar />
+        <Container sx={{textAlign: 'left', paddingTop: '10px'}}>
+          <Homepage />
+        </Container>
+      </div>
+    );
+  } else {
+    return (
+      <div className="App">
+        <Navbar />
+        <Container sx={{textAlign: 'left', paddingTop: '10px'}}>
+          <Alert severity="error">
+            <AlertTitle>Error - Missing API token</AlertTitle>
+            The <strong>OpenWeatherMap forecast api token</strong> is <strong>missing</strong>.
+            <br />
+            <br />
+            Steps to fix: 
+            <ul>
+              <li>Create a <strong>.env</strong> file in the application root (outside of src directory)</li>
+              <li>Create a variable called <strong>REACT_APP_OPENWEATHER_TOKEN</strong></li>
+              <li>Save the token inside REACT_APP_OPENWEATHER_TOKEN</li>
+            </ul>
+            <br />
+            Example:
+            <br />
+
+            <code>
+              REACT_APP_OPENWEATHER_TOKEN="6863f057deaf11250e7b54465a0fe53f"
+            </code>
+          </Alert>
+        </Container>
+      </div>
+    );
+  }
 }
 
 export default App;
