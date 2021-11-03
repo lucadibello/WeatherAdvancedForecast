6f2ddbcb498ca318629a0288b6f709e40329229d
luca.dibello@student.supsi.ch <info@lucadibello.ch>
2021-11-03 13:09:25 +0100

    Homepage updated

--- src/components/Homepage.tsx
+++ src/components/Homepage.tsx
@@ -1,15 +1,75 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 
 import {
-  Typography
+  Alert,
+  Box,
+  Typography,
+  Button
 } from '@mui/material'
 
+import { UserLocation } from '../models/UserLocation';
+import * as LocationService from '../services/Location';
+import * as WeatherService from '../services/Weather';
+
+
 export default function Homepage () {
+  const [location, setLocation] = React.useState<UserLocation | null>(null);
+
+  const nearbyCities = () => {
+    if (location != null) {
+      WeatherService.getNearbyCities(
+        location.latitude,
+        location.longitude,
+        10
+      );
+    }
+  }
+
+  // Try to fetch location
+  useEffect(() => {
+    LocationService.getUserLocation((position: GeolocationPosition) => {
+      setLocation({
+        latitude: position.coords.latitude,
+        longitude: position.coords.longitude,
+        altitude: position.coords.altitude,
+        _accuracy: position.coords.accuracy
+      })
+    })
+  })
+
+  nearbyCities();
+
   return (
-    <Typography sx={{
-      color: 'black'
-    }} variant='h6' component='h1'>
-      Ciao! API KEY: {process.env.REACT_APP_OPENWEATHER_TOKEN}
-    </Typography>
+    <Box>
+      {/* Ask user to enable location */}
+      {location === null && 
+        <Alert severity="error">
+          Enable your location to get the information you need in less time :)
+          <Button variant="outlined" color="error"  onClick={() => {
+            LocationService.getUserLocation((position: GeolocationPosition) => {
+              setLocation({
+                latitude: position.coords.latitude,
+                longitude: position.coords.longitude,
+                altitude: position.coords.altitude,
+                _accuracy: position.coords.accuracy
+              })
+            })
+          }} sx={{
+            marginLeft: "10px"
+          }}>Enable location</Button>
+        </Alert>
+      }
+      
+      <Typography sx={{
+        color: 'black'
+      }} variant='h6' component='h1'>
+        Ciao! API KEY: {process.env.REACT_APP_OPENWEATHER_TOKEN}
+      </Typography>
+
+
+      <Typography variant='h6' component='h1'>
+        Current position: {JSON.stringify(location)}
+      </Typography>
+    </Box>
   );
 }
