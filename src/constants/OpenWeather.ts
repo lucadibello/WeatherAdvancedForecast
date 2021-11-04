eadd3894234253cac8871a9652ec25c44c108e08
luca.dibello@student.supsi.ch <info@lucadibello.ch>
2021-11-04 15:05:48 +0100

    Added geocode API url to constants

--- src/constants/OpenWeather.ts
+++ src/constants/OpenWeather.ts
@@ -1,7 +1,9 @@
 const API_FORECAST_URL = "http://api.openweathermap.org/data/2.5/forecast";
 const API_GEOCODE_REVERSE_URL = "http://api.openweathermap.org/geo/1.0/reverse";
+const API_GEOCODE_URL = "http://api.openweathermap.org/geo/1.0/direct";
 
 export {
   API_FORECAST_URL,
-  API_GEOCODE_REVERSE_URL
+  API_GEOCODE_REVERSE_URL,
+  API_GEOCODE_URL
 }
