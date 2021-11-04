77bd5a5fdc8db490ce9d9bc5a71398d5d7f12f5d
luca.dibello@student.supsi.ch <info@lucadibello.ch>
2021-11-04 15:05:13 +0100

    Added getCoordinates method

--- src/services/WeatherService.ts
+++ src/services/WeatherService.ts
@@ -1,8 +1,20 @@
-import { API_GEOCODE_REVERSE_URL, API_FORECAST_URL } from "../constants/OpenWeather"
+import { API_GEOCODE_REVERSE_URL, API_FORECAST_URL, API_GEOCODE_URL } from "../constants/OpenWeather"
 import NearbyCity from "../models/NearbyCity";
 
 async function getNearbyCities(lat:number, lon: number, nCities: number): Promise<NearbyCity[]> {
   return await fetch(API_GEOCODE_REVERSE_URL+`?lat=${lat}&lon=${lon}&limit=${nCities}&appid=${process.env.REACT_APP_OPENWEATHER_TOKEN}`)
+    .then(response => response.json())
+}
+
+async function getCoordinates (city: NearbyCity | string): Promise<NearbyCity[]> {
+  let cityName;
+  if (typeof city == "string") {
+    cityName = city;
+  } else {
+    cityName = city.name;
+  }
+  
+  return await fetch(API_GEOCODE_URL+`?q=${cityName}&appid=${process.env.REACT_APP_OPENWEATHER_TOKEN}`)
     .then(response => response.json())
 }
 
