1045b20535630d95c4d6602b94f16e810e61fa1f
luca.dibello@student.supsi.ch <info@lucadibello.ch>
2021-11-04 13:47:26 +0100

    Updated API url

--- src/constants/OpenWeather.ts
+++ src/constants/OpenWeather.ts
@@ -1,4 +1,4 @@
-const API_FORECAST_URL = "http://pro.openweathermap.org/data/2.5/forecast";
+const API_FORECAST_URL = "http://api.openweathermap.org/data/2.5/forecast";
 const API_GEOCODE_REVERSE_URL = "http://api.openweathermap.org/geo/1.0/reverse";
 
 export {
